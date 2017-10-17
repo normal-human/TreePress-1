722c1c0c87229ad5dd297d917a4d0d4a2f028e13
LAPTOP-2IBCVPSH\Lawrence Bitzer <lawrencebitzer@gmail.com>
2017-10-17 15:10:06 -0400

    First Commit

---
+++ HomePageJS.js
@@ -0,0 +1,27 @@
+$("#Home").click(function(){
+    window.open('https://www.google.com/', 'Home');
+});
+
+$("#link1").click(function(){
+    window.open('https://www.google.com/', 'link');
+});
+
+$("#link2").click(function(){
+    window.open('https://www.google.com/', 'link');
+});
+
+$("#link3").click(function(){
+    window.open('https://www.google.com/', 'link');
+});
+
+$("#link4").click(function(){
+    window.open('https://www.google.com/', 'link');
+});
+
+$("#twitter").click(function(){
+    window.open('https://twitter.com/ghalcove', 'twitter');
+});
+
+$("#facebook").click(function(){
+    window.open('https://www.facebook.com/ghalcove/', 'facebook');
+});
